b182d072b9349ac17a2b8b33b3a59beef5dee475
gkcatoon-hash <gkcatoon@gmail.com>
2026-09-01 09:48:11 +0530

    Add MainActivity for GKVG Calendar app

---
+++ java/com/gkvg/calendar/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.gkvg.calendar
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val textView = TextView(this)
+        textView.text = "GKVG Calendar"
+        textView.textSize = 28f
+        textView.setPadding(40, 80, 40, 40)
+
+        setContentView(textView)
+    }
+}
